94a87db77e2c5c66b7ec2a9c4f5d1567b168d465
George Roots <groots@squishdesigns.com>
2015-04-14 23:10:06 -0400

    added more request handliers for user signin and user page

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,7 @@ app.use(function(req, res, next){
 	next();
 });
 
+//request handler to authenticate user
 app.post('/authenticate', function(req, res){
 	User.findOne({email: req.body.email, password: req.body.password, function(err, user)})
 	if(err){
@@ -41,5 +42,56 @@ app.post('/authenticate', function(req, res){
 			}
 		}
 
+	});
+});
+
+//request handler for sign in
+app.post('/signin', function(req, res){
+	User.findOne({
+		email: req.body.email, password: req.body.password, function(err, user){
+			if(err){
+				res.json({
+					type: false,
+					data: "Error occured " + err
+				})
+			} else {
+				if(user){
+					res.json({
+						type: false,
+						data: "User already exists!"
+					});
+				} else {
+					var userModel = new User();
+					userModel.email = req.body.email;
+					userModel.password = req.body.email
+					userModel.save(function(err, user){
+						user.token = jwt.sign(user, process.env.JWT_SECRET);
+						user.save(function(err, user1){
+							res.json({
+								type: true,
+								data: user1,
+								token: user1.token
+							});
+						});
+					});
+				}
+			}
+		}
+	});
+});
+
+app.get('/me', ensureAuthorized, function(req, res){
+	User.findOne({token: req.token}, function(err, user){
+		if(err){
+			res.json({
+				type: false,
+				data: "Error occured" + err
+			});
+		} else {
+			res.json({
+				type: true,
+				data: user
+			});
+		}
 	});
 });
